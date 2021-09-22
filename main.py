85246f56b3ed6fc10aea27cb9341da8efd2f4427
Ardi <ardiasllani@hotmail.com>
2021-09-21 22:52:35 -0400

    P12

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """
 Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
-Resources: From textbook.ds100.org: 6.3 SQL Joins
+Resources: From stackoverflow.com
 """
 
 import pandas as pd
 import pandasql as psql
 
 # get file names:
-# input_file_ = input('Please enter input file name: ')
-# output_file_ = input('Please enter output file name: ')
+input_file_ = input('Please enter input file name: ')
+output_file_ = input('Please enter output file name: ')
 
-input_file_ = "mta_trunc_staten_island.csv"
-output_file_ = "filteredSI.csv"
+# input_file_ = "mta_trunc_staten_island.csv"
+# output_file_ = "filteredSI.csv"
 
 # read csv file
 df_ = pd.read_csv(input_file_)
@@ -21,8 +21,3 @@ results_ = df_.groupby(['date']).agg(
     {'entries': 'sum', 'exits': 'sum'}).reset_index()
 
 results_.to_csv(output_file_, index=False)
-
-# date,entries,exits
-# 2020-01-01,3128,0
-# 2020-01-02,13707,0
-# 2020-01-03,12507,23
