bec1cd22d35908af6728aef6345105d0c511c345
Ardi <ardiasllani@hotmail.com>
2021-09-24 02:15:36 -0400

    (value-1)

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ def cyclicAverage(column):
         count_ = 1
         sum_ = column[value]
         if(value - 7 > 0):
-            sum_ += column[value-7]
+            sum_ += column[(value-1)-7]
             count_ += 1
         if(value - 14 > 0):
-            sum_ += column[value-14]
+            sum_ += column[(value-1)-14]
             count_ += 1
         avg_ = float(sum_ / count_)
         list_.append(avg_)
