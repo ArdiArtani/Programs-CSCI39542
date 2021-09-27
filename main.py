3b44c0c64b5304d87db2114acb37324790b00a43
Ardi <ardiasllani@hotmail.com>
2021-09-26 23:31:47 -0400

    P14

--- main.py
+++ main.py
@@ -1,54 +1,27 @@
 """
 Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
-Resources: stackoverflow, python
+Resources: stackoverflow
 """
-# import pandas as pd
+import pandas as pd
+import re
 
-# cumulativeAverage(column): Assumes the input is a Series of numerical data. Returns a Series with the cumulative (running) average of the values. For example, if the first 5 entries are 10,20,30,40,20, the Series that is returned would start out as 10,15,20,25,24 (since 10/1 = 10, (10+20)/2 = 15, (10+20+30)/3 = 20, (10+20+30+40)/4 = 25, (10+20+30+40+20)/5 = 24). Note that it is different that rolling() we used in class, since this function creates a value for all entries and averages across all values seen.
-def cumulativeAverage(column):
-    list_ = []
-    count_ = 0
-    sum_ = avg_ = 0.0
+# Write two functions that will be used to clean the OpenData NYC dataset of Libraries in New York City (downloaded as CSV file). The first three lines of the CSV file look like:
 
-    for value in column:
-        sum_ += value
-        avg_ = sum_ /(count_ + 1)
-        count_ += 1
-        list_.append(avg_)
-    return list_
+# This function takes the values from the column the_geom and extracts the longitude and latitude from the string (they are surrounded by parenthesis and separated by a space, and returns the two as numerical values. For example, the function would return -73.95353074430393, 40.80297988196676 when applied to the first row of data.
+def extractLatLon(row):
 
-# cyclicAverage(column): Assumes the input is a Series of numerical data. Returns a Series with the average of the current day with, if they exist, the value from 7 days previously and 14 days previously. That is, if they exist, for entry at index i, take the average of the values at indices i-offset, i-2*offset, and i-3*offset, as the computation. Since ridership is highly dependent on the day of the week, this averages the values of the same day in past weeks.
-def cyclicAverage(column):
-    list_ = []
-    sum_ = avg_ = 0.0
+    regex_ = r'POINT \((.*?) (.*)\)'
+    lat_, lon_ = re.search(regex_, row).groups()
 
-    for value in range(len(column)):
-        count_ = 1
-        sum_ = column[value]
-        if(value - 7 > 0):
-            sum_ += column[value-7]
-            count_ += 1
-        if(value - 14 > 0):
-            sum_ += column[value-14]
-            count_ += 1
-        avg_ = float(sum_) / float(count_)
-        list_.append(avg_)
-    return list_
+    return lat_ + ", " +lon_
 
-# exponentialSmoothing(column): Assumes the input is a Series of numerical data. Returns a Series with a weighted average of the previous values with the most recent values have higher weight and the older ones have lower weights. The value for the first entry, newCol[0] is column[0]. The value for subsequent entries is newCol[t+1] = 0.5*column[t+1] + 0.5*newCol[t]. For example, if we had the same Series starting the same as above, 10,20,30,40,20, our new column would start 10,15,22.5,31.25,25.625 (since the first entry is the same, 0.5*20+0.5*10 = 15, 0.5*30+0.5*15= 22.5, 0.5*40+0.5*22.5 = 31.25, 0.5*20 + 0.5*31.25 = 25.625).
-def exponentialSmoothing(column):
-    list_ = [column[0]]
-    alpha_ = .5
+# extractTitle(row): This function concatenates the values from the columns NAME, CITY, and ZIP code into a single string, separated by a comma and space, and returns the string (to be used as the title for our visualizations). For example, when applying this function to the first data row, the return value would be: 115th Street, New York, 10026.
+def extractTitle(row):
 
-    for value in range(1, len(column)):
-        newCol_ = alpha_ * column[value] + (1 - alpha_) * list_[value - 1]
-        list_.append(newCol_)
+    r_data_ = row.split(',')
+    name_ = r_data_[1]
+    city_ = r_data_[4]
+    zip_ = r_data_[5]
 
-    return list_
-
-
-# column = [10,20,30,40,20]
-# print(column)
-# print(exponentialSmoothing(column))
-# 10,15,22.5,31.25,25.625
+    return name_ + ', ' + city_ + ', ' + zip_
