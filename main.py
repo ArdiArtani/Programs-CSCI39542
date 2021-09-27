ed02c51e79b2158ee253e0a1dca5e1880790ad36
Ardi <ardiasllani@hotmail.com>
2021-09-26 23:46:47 -0400

    P14

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ import re
 # This function takes the values from the column the_geom and extracts the longitude and latitude from the string (they are surrounded by parenthesis and separated by a space, and returns the two as numerical values. For example, the function would return -73.95353074430393, 40.80297988196676 when applied to the first row of data.
 def extractLatLon(row):
 
-    df_ = pd.read_csv(row)
-    row_ = df_['the_geom']
-    regex_ = r'POINT \((.*?) (.*)\)'
-    lat_, lon_ = re.search(regex_, row_).groups()
+    # coords = row['the_geom'][7:-2].split()
+    # res = (float(coords[0]), float(coords[1]))
 
-    return lat_ + ", " +lon_
+    regex_ = r'POINT \((.*?) (.*)\)'
+    lat_, lon_ = re.search(regex_, row).groups()
+
+    return lat_ + ", " + lon_
 
 # extractTitle(row): This function concatenates the values from the columns NAME, CITY, and ZIP code into a single string, separated by a comma and space, and returns the string (to be used as the title for our visualizations). For example, when applying this function to the first data row, the return value would be: 115th Street, New York, 10026.
 def extractTitle(row):
@@ -29,4 +30,8 @@ def extractTitle(row):
     return name_ + ', ' + city_ + ', ' + zip_
 
 
-# POINT (-73.95353074430393 40.80297988196676),115th Street,West 115th Street,203,New York,10026,http://www.nypl.org/locations/115th-street,1055236,1018310026,997115.12977,231827.652864,NYPL,1
+
+
+# row = "POINT (-73.95353074430393 40.80297988196676),115th Street,West 115th Street,203,New York,10026,http://www.nypl.org/locations/115th-street,1055236,1018310026,997115.12977,231827.652864,NYPL,1"
+# print(extractLatLon(row))
+# print(extractTitle(row))
