5ec9dabb5156947eecad7241dddebdda2cfe3fbe
Ardi <ardiasllani@hotmail.com>
2021-09-27 22:30:35 -0400

    P15

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ Email: ARDI.ARTANI96@myhunter.cuny.edu
 Resources: stackoverflow
 """
 import pandas as pd
-import re
-
-# Write two functions that will be used to clean the OpenData NYC dataset of Libraries in New York City (downloaded as CSV file). The first three lines of the CSV file look like:
-
-# This function takes the values from the column the_geom and extracts the longitude and latitude from the string (they are surrounded by parenthesis and separated by a space, and returns the two as numerical values. For example, the function would return -73.95353074430393, 40.80297988196676 when applied to the first row of data.
-def extractLatLon(row):
-    regex_ = r'POINT \((.*?) (.*)\)'
-    geom_ = str(row['the_geom'])
-    lat_, lon_= re.search(regex_, geom_).groups()
-    return (float(lat_), float(lon_))
+import matplotlib.pyplot as plt
 
 
-# extractTitle(row): This function concatenates the values from the columns NAME, CITY, and ZIP code into a single string, separated by a comma and space, and returns the string (to be used as the title for our visualizations). For example, when applying this function to the first data row, the return value would be: 115th Street, New York, 10026.
-def extractTitle(row):
-    return str(row['NAME']) + ', ' + str(row['CITY']) + ', ' + str(row['ZIP'])
+# The goal is to create a plot of NYC OpenData Motor Vehicle Collisions that follows this style. For example, here is the plot for January 2020 dataset:
 
+# Your program should begin by asking the user for input and output files. It should be written to take any dataset from the NYC OpenData Motor Vehicle Collisions and produce an image that matches this style. The resulting image should be saved to the output file specified by the user.
 
+# get file names:
+input_file_ = input('Please enter input file name: ')
+output_file_ = input('Please enter output file name: ')
 
-# Load the csv of libary locations:
-# lib = pd.read_csv('LIBRARY.csv')
-# print(extractLatLon(lib))
+# input_file_ = "Motor_Vehicle_Collisions_Mar_2020.csv"
+# output_file_ = "output.csv"
+
+# read csv files
+df = pd.read_csv(input_file_)
+boroDF = df.groupby(['BOROUGH','CRASH DATE']).count()['CRASH TIME'].unstack().transpose()
+
+boroDF.plot()
+plt.show()
