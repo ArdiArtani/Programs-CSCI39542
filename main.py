a7a34be8956caed6f1667ca7106afb90ee19132b
Ardi <ardiasllani@hotmail.com>
2021-09-27 23:11:43 -0400

    p15

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 # Your program should begin by asking the user for input and output files. It should be written to take any dataset from the NYC OpenData Motor Vehicle Collisions and produce an image that matches this style. The resulting image should be saved to the output file specified by the user.
 
 # get file names:
-input_file_ = input('Please enter input file name: ')
-output_file_ = input('Please enter output file name: ')
+# input_file_ = input('Please enter input file name: ')
+# output_file_ = input('Please enter output file name: ')
 
-# input_file_ = "Motor_Vehicle_Collisions_Mar_2020.csv"
+input_file_ = "Motor_Vehicle_Collisions_Mar_2020.csv"
 # output_file_ = "output.png"
 
 # read csv files
@@ -22,10 +22,11 @@ df = pd.read_csv(input_file_)
 boroDF = df.groupby(['BOROUGH', 'CRASH DATE']).count()[
                     'CRASH TIME'].unstack().transpose()
 
+plt.subplots(figsize=(12, 8))
 plt.title('Collisions in New York City')
 plt.suptitle('By borough and date in code')
 # Did not find CSci 39542, Hunter College as a course name and Source: OpenData NYC in code as data source
 
 boroDF.plot()
 plt.show()
-plt.savefig(output_file_)
+# plt.savefig(output_file_)
