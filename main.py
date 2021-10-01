29dead93a8541858f68814fc1fdd73c608a560a0
Ardi <ardiasllani@hotmail.com>
2021-10-01 11:54:39 -0400

    p17

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ df_math_['District'] = df_math_['DBN'].apply(extractDistrict)
 df_ela_['Proficiency'] = (df_ela_['# Level 3+4']/df_ela_['Number Tested']) * 100
 df_math_['Proficiency'] = (df_math_['# Level 3+4']/df_math_['Number Tested']) * 100
 
+# add in the schools column
+df_ela_['School Name'] = df_ela_['School Name']
+df_math_['School Name'] = df_math_['School Name']
+
 #  group and get maxium profiency
 df_ela_ = df_ela_.loc[ df_ela_.groupby('District')['Proficiency'].idxmax() ]
 df_math_ = df_math_.loc[ df_math_.groupby('District')['Proficiency'].idxmax() ]
@@ -32,8 +36,8 @@ df1_ = df_ela_.reindex(columns = ['District', 'Subject', 'Proficiency', 'School 
 df2_ = df_math_.reindex(columns = ['District', 'Subject', 'Proficiency', 'School Name'])
 
 # concat both csv filess
-df_ = pd.concat([df1_,df2_], axis=0)
+df = pd.concat([df1_,df2_], axis=0)
 
 # create pivot table
-df_ = pd.pivot_table(df_, index=['District','Subject'], aggfunc=max)
+df_ = pd.pivot_table(df, index=['District','Subject'], aggfunc=max)
 print(df_)
