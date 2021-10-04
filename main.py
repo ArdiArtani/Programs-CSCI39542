9647b2a4d0a88139f7f3aec12545785b2d5be604
Ardi <ardiasllani@hotmail.com>
2021-10-04 13:09:29 -0400

    p19

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import scipy.stats as stats
 # computeSmoothing(xes,points): This function takes a numpy array xes and a list, points, of numeric values. For each p in points, the function should compute the normal probability distribution function (scipy.norm.pdf) centered at loc = p with standard deviation scale = 0.5 for all values in xes. The return value is a numpy array of the sum of these at each point.
 def computeSmoothing(xes, points):
     values_ = list(xes)
-    sum_ = 0
+    sum_ = 0.0
     for p_ in range(points[0]):
         # print(values_[p_])
-        sum_ += stats.norm.pdf(values_[p_], p_, 0.5)
+        sum_ = stats.norm.pdf(values_, p_, 0.5)
     return sum_
 
 
