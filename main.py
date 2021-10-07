e9f1eb07e20a263703fcc506faf3ebf327a85b66
Ardi <ardiasllani@hotmail.com>
2021-10-07 00:47:07 -0400

    p21

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
 """
 Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
-Resources: textbook.ds100.org, datatechnotes.com/2019/10/accuracy-check-in-python-mae-mse-rmse-r.html
+Resources: n/a
 """
-import numpy as np
-import scipy.stats as stats
-import matplotlib.pyplot as plt
+import pandas as pd
 
-def mse_loss(theta, y_vals):
-    return np.mean((y_vals - theta) ** 2)
+# The program should open the file name provided by the user. Next, the program should copy the input file and create two new columns: percent_tip, which is 100*tip_amount/fare_amount and percent_fare, which is 100*fare_amount/total_amount. Your program should write out a new CSV file (with the name provided by the user) with the original columns as well as the two newly computed ones.
+input_ = input('Enter input file name:')
+output_ = input('Enter output file name:')
+# input_ = 'taxi_new_years_day_2020.csv'
+# output_ = 'taxi_Jan2020_with_percents.csv'
 
-def mae_loss(theta, y_vals):
-    return np.mean(abs(y_vals - theta))
+df = pd.read_csv(input_)
 
-# thetas: estimates for the population parameter for the percent tips in values, and
-# tips: the tips observed, assumed to be a positive percentage, ranging from 0 to 100.
-# For each theta in thetas, it should compute the mean squared error between theta and tips. Return an iterable of the values computed.
-def mse_estimates(thetas,tips):
-    values_ = []
-    for theta_ in thetas:
-        value_ = mse_loss(theta_, tips)
-        values_.append(value_)
-    return list(values_)
+df['percent_tip'] = (df['tip_amount'] * 100) / df['fare_amount']
+df['percent_fare'] = (df['fare_amount'] * 100) / df['total_amount']
 
-
-# thetas: estimates for the population parameter for the percent tips in values, and
-# tips: the tips observed, assumed to be a positive percentage, ranging from 0 to 100.
-# For each theta in thetas, it should compute the mean absolute error between theta and tips. Return an iterable of the values computed.
-# Note: for each of these functions, your returned value will be an iterable with the same length as thetas.
-def mae_estimates(thetas,tips):
-    values_ = []
-    for theta_ in thetas:
-        value_ = mae_loss(theta_, tips)
-        values_.append(value_)
-    return list(values_)
-
-# thetas = np.array([12, 13, 14, 15, 16, 17])
-# y_vals = np.array([12.1, 12.8, 14.9, 16.3, 17.2])
-# mse_losses = mse_estimates(thetas,y_vals)
-# abs_losses = mae_estimates(thetas,y_vals)
-# plt.scatter(thetas, mse_losses, label='MSE')
-# plt.scatter(thetas, abs_losses, label='MAE')
-# plt.title(r'Loss vs. $ \theta $ when $ \bf{y}$$= [ 12.1, 12.8, 14.9, 16.3, 17.2 ] $')
-# plt.xlabel(r'$ \theta $ Values')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.show()
+df.to_csv(output_, index=False)
