70eb1c360264503cd019b794c954d02be1fc1e13
Ardi <ardiasllani@hotmail.com>
2021-10-15 00:15:50 -0400

    p23

--- main.py
+++ main.py
@@ -17,20 +17,25 @@ import numpy as np
 # The function computes the correlation coefficient between df[colName] and each column specified in colList, and returns the name and the correlation coefficient for the column from the list with the highest absolute value.
 def findHighestCorr(colName,colLst,df):
 
-    cor_results_ = []
+    corr_results_ = []
     for col_name_ in colLst:
-        if(colName != col_name_):
-            cor_results_.append(abs(df[colName].corr(df[col_name_])))
+        # if(colName != col_name_):
+        corr_results_.append(abs(df[colName].corr(df[col_name_])))
 
-    highest_value_ = np.amax(cor_results_)
-    return (colName, highest_value_)
+    highest_value_ = np.amax(corr_results_)
+    return (colLst[corr_results_.index(highest_value_)], highest_value_)
     # return f"{colName} has highest absolute r with {highest_value_}"
 
 
-# simpleDF = pd.DataFrame({'c1': [1,2,3,4],\
-#                          'c2': [0,1,0,1],\
-#                          'c3': [1,10,3,20],\
-#                          'c4': [-10,-20,-30,-40],})
-# print('Testing with c1 and [c3,c4]:')
-# print(findHighestCorr('c1',['c3','c4'],simpleDF))
-# print(f'c1 has highest absolute r with {findHighestCorr("c1",simpleDF.columns, simpleDF)}.')
+
+simpleDF = pd.DataFrame({'c1': [1,2,3,4],\
+                         'c2': [0,1,0,1],\
+                         'c3': [1,10,3,20],\
+                         'c4': [-10,-20,-30,-40],})
+print('Testing with c1 and [c3,c4]:')
+print(findHighestCorr('c1',['c3','c4'],simpleDF))
+print(f'c1 has highest absolute r with {findHighestCorr("c1",simpleDF.columns, simpleDF)}.')
+
+# Testing with c1 and [c3,c4]:
+# ('c4', -1.0)
+# c1 has highest absolute r with ('c1', 1.0)
