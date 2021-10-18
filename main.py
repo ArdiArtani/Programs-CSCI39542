6b7a22e07198b4fec92698f5f83b239437585b61
Ardi <ardiasllani@hotmail.com>
2021-10-18 11:48:23 -0400

    p24

--- main.py
+++ main.py
@@ -5,34 +5,33 @@ Resources: n/a
 """
 
 import pandas as pd
-import numpy as np
-
-# This function takes three inputs: colName: a column name of the specified DataFrame,
-# colList: a list of column names of the specified DataFrame. It must be a list and assumed to be non-empty. Can include colName, and
-# df: a DataFrame including the specified columns.
-# The function computes the correlation coefficient between df[colName] and each column specified in colList, and returns the name and the correlation coefficient for the column from the list with the highest absolute value.
 
 
-def findHighestCorr(colName, colLst, df):
-    y_value_ = 0
-    index_ = ''
-    for col_name_ in colLst:
-        x_value_ = df[colName].corr(df[col_name_])
-        if abs(x_value_) > abs(y_value_):
-            y_value_ = x_value_
-            index_ = col_name_
-    return index_, y_value_
+def computeEnrollments(df):
+
+    # only students taking 3 or more courses
+    num_courses_ = df['Current Courses'].str.split().apply(len)
+
+    # the first that counts total number of courses the student is taking
+    df['NumCourses'] = num_courses_
+    results_ = df.loc[df.NumCourses >= 3]
+
+    # the second columns has the number of computer science courses currently taking (all courses that start 'csci')
+    results_['CS'] = results_['Current Courses'].str.findall('csci*').count()
+
+    # third column with the number of other courses the student is taking.
+    results_['Other'] = (results_['NumCourses'] - results_['CS'])
+
+    return results_
 
 
-# simpleDF = pd.DataFrame({'c1': [1, 2, 3, 4],
-#                          'c2': [0, 1, 0, 1],
-#                          'c3': [1, 10, 3, 20],
-#                          'c4': [-10, -20, -30, -40], })
-# print('Testing with c1 and [c3,c4]:')
-# print(findHighestCorr('c1', ['c3', 'c4'], simpleDF))
-# print(
-#     f'c1 has highest absolute r with {findHighestCorr("c1",simpleDF.columns, simpleDF)}.')
-
-# Testing with c1 and [c3,c4]:
-# ('c4', -1.0)
-# c1 has highest absolute r with ('c1', 1.0)
+# classDF = pd.DataFrame({'Name': ["Ana", "Bao", "Cara", "Dara", "Ella", "Fatima"],
+#                         '# Credits': [45, 50, 80, 115, 30, 90],
+#                         'Current Courses': ["csci160 csci235 math160 jpn201",
+#                                             "csci160 csci235 cla101 germn241",
+#                                             "csci265 csci335 csci39542 germn241",
+#                                             "csci49362 csci499",
+#                                             "csci150 csci235 math160",
+#                                             "csci335 csci39542 cla101 dan102"]})
+# print(f'Starting df:\n {classDF}')
+# print(f'Ending df:\n {computeEnrollments(classDF)}')
