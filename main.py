f10ddc0e3070fb0c7a793d0a48278e05a501d45e
Ardi <ardiasllani@hotmail.com>
2021-10-19 11:35:29 -0400

    p25

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 """
 Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
-Resources: stackoverflow.com
+Resources: n/a
 """
 import pandas as pd
-
-def computeEnrollments(df):
-
-    # only students taking 3 or more courses
-    num_courses_ = df['Current Courses'].str.split().apply(len)
-
-    # the first that counts total number of courses the student is taking
-    df['NumCourses'] = num_courses_
-    results_ = df.loc[df.NumCourses >= 3]
-
-    # the second columns has the number of computer science courses currently taking (all courses that start 'csci')
-    new_csci_ = results_['Current Courses'].str.findall('csci*')
-    results_['CS'] = new_csci_.str.len()
-
-    # third column with the number of other courses the student is taking.
-    results_['Other'] = (results_['NumCourses'] - results_['CS'])
-
-    return results_
+import matplotlib.pyplot as plt
 
 
-# classDF = pd.DataFrame({'Name': ["Ana", "Bao", "Cara", "Dara", "Ella", "Fatima"],
-#                         '# Credits': [45, 50, 80, 115, 30, 90],
-#                         'Current Courses': ["csci160 csci235 math160 jpn201",
-#                                             "csci160 csci235 cla101 germn241",
-#                                             "csci265 csci335 csci39542 germn241",
-#                                             "csci49362 csci499",
-#                                             "csci150 csci235 math160",
-#                                             "csci335 csci39542 cla101 dan102"]})
-# print(f'Starting df:\n {classDF}')
-# print(f'Ending df:\n {computeEnrollments(classDF)}')
+# Section 16.1 (Random Variables) of the textbook has a small example of computing the probability mass function of a data set of ages was computed by hand. Write a function that will automate this process:
+
+# uniqueVals: a sorted list of the unique values from the input parameter vals, and
+# mass: the fraction that each uniqueVals occurs in vals.
+def pmf(vals):
+    uniqueVals_ = set(vals)
+    mass_ = []
+    for val_ in uniqueVals_:
+        mass_.append(vals.count(val_) / len(vals))
+
+    return uniqueVals_, mass_
+
+x, y = pmf([50,50,52,54])
+print(f'The values are: {x}')
+print(f'The pmf is: {y}')
+print(f'The sum of the pmf is: {sum(y)}.')
+plt.bar(x,y)
+plt.show()
+
+# The values are: (50, 52, 54)
+# The pmf is: (0.5, 0.25, 0.25)
+# The sum of the pmf is: 1.0.
