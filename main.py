f4007089216f101d15e670e2e55a613edf17b579
Ardi <ardiasllani@hotmail.com>
2021-10-20 23:07:56 -0400

    p26

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 """
 Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
-Resources: stackoverflow
+Resources: pandas.pydata.org
 """
+import seaborn as sns
 import pandas as pd
 
-# uniqueVals: a sorted list of the unique values from the input parameter vals, and
-# mass: the fraction that each uniqueVals occurs in vals.
-def pmf(vals):
-    uniqueVals_ = list(set(vals))
-    mass_ = []
+# tripTime(start,end): This function takes two variables of type datetime and returns the difference between them.
+def tripTime(start, end):
+    # convert date to datatime
+    start_dt1_ = pd.to_datetime(start)
+    end_dt2_ = pd.to_datetime(end)
+    return (end_dt2_ - start_dt1_)
 
-    if (isinstance(vals, pd.core.series.Series)):
-        vals = vals.tolist()
-    else:
-        vals = vals
-
-    for val_ in uniqueVals_:
-        mass_.append(vals.count(val_) / len(vals))
-
-    return uniqueVals_, mass_
+# weekdays(df,col): This function takes a DataFrame, df, containing the column name, col, and returns a DataFrame containing only times that fall on a weekday (i.e. Monday through Friday).
 
 
-# senators = pd.read_csv('senatorsAges.csv')
-# x, y = pmf(senators['age'])
-# x, y = pmf([50,50,52,54])
-# print(f'The values are: {x}')
-# print(f'The pmf is: {y}')
-# print(f'The sum of the pmf is: {sum(y)}.')
+def weekdays(df, col):
+    df_ = pd.to_datetime(df[col])
+    results_ = df[df_.dt.dayofweek < 5]
+    return results_
 
-# The values are: (50, 52, 54)
-# The pmf is: (0.5, 0.25, 0.25)
-# The sum of the pmf is: 1.0.
+
+# pickup,dropoff,passengers,distance,fare,tip,tolls,total,color,payment,pickup_zone,dropoff_zone,pickup_borough,dropoff_borough
+
+# taxi = sns.load_dataset('taxis')
+# print(taxi.iloc[0:10])  # Print first 10 lines:
+# taxi['tripTime'] = taxi.apply(
+#     lambda x: tripTime(x['pickup'], x['dropoff']), axis=1)
+# print(taxi.iloc[0:10])
+
+#                 pickup              dropoff  ...  pickup_borough  dropoff_borough
+# 0  2019-03-23 20:21:09  2019-03-23 20:27:24  ...       Manhattan        Manhattan
+# 1  2019-03-04 16:11:55  2019-03-04 16:19:00  ...       Manhattan        Manhattan
+
+#                 pickup              dropoff  ...  dropoff_borough        tripTime
+# 0  2019-03-23 20:21:09  2019-03-23 20:27:24  ...        Manhattan 0 days 00:06:15
+# 1  2019-03-04 16:11:55  2019-03-04 16:19:00  ...        Manhattan 0 days 00:07:05
