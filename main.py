6c0cbd75753095de396f8a625cb7f793cb29b011
Ardi <ardiasllani@hotmail.com>
2021-10-22 11:29:51 -0400

    p27

--- main.py
+++ main.py
@@ -3,39 +3,33 @@ Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
 Resources: pandas.pydata.org
 """
-import seaborn as sns
 import pandas as pd
+import numpy as np
+# import statistics as st
 
-# tripTime(start,end): This function takes two variables of type datetime and returns the difference between them.
+# Write a function, compute_r_line(), that takes two iterables of numeric values representing the independent variable (xes) and the dependent variable (yes) and computes the slope and y-intercept of the linear regression line using ordinary least squares. See DS 8: Chapter 15 The pseudocode for this:
+#
+# Compute the standard deviation of the xes and yes. Call these sd_x and sd_y.
+# Compute the correlation, r, of the xes and yes.
+# Compute the slope, m, as m = r*sd_y/sd_x.
+# Compute the y-intercept, b, as b = yes[0] - m * xes[0]
+# Return m and b.
 
+def compute_r_line(xes, yes):
+    sd_x = np.std(xes)
+    sd_y = np.std(yes)
+    r = np.corrcoef(sd_x, sd_y)
+    m = r * sd_y / sd_x
+    b = yes[0] - m * xes[0]
+    return m, b
 
-def tripTime(start, end):
-    # convert date to datatime
-    start_dt1_ = pd.to_datetime(start)
-    end_dt2_ = pd.to_datetime(end)
-    return (end_dt2_ - start_dt1_)
-
-# weekdays(df,col): This function takes a DataFrame, df, containing the column name, col, and returns a DataFrame containing only times that fall on a weekday (i.e. Monday through Friday).
-
-
-def weekdays(df, col):
-    df_ = pd.to_datetime(df[col])
-    results_ = df[df_.dt.dayofweek < 5]
-    return results_
-
-
-# pickup,dropoff,passengers,distance,fare,tip,tolls,total,color,payment,pickup_zone,dropoff_zone,pickup_borough,dropoff_borough
-
-# taxi = sns.load_dataset('taxis')
-# print(taxi.iloc[0:10])  # Print first 10 lines:
-# taxi['tripTime'] = taxi.apply(
-#     lambda x: tripTime(x['pickup'], x['dropoff']), axis=1)
-# print(taxi.iloc[0:10])
-
-#                 pickup              dropoff  ...  pickup_borough  dropoff_borough
-# 0  2019-03-23 20:21:09  2019-03-23 20:27:24  ...       Manhattan        Manhattan
-# 1  2019-03-04 16:11:55  2019-03-04 16:19:00  ...       Manhattan        Manhattan
-
-#                 pickup              dropoff  ...  dropoff_borough        tripTime
-# 0  2019-03-23 20:21:09  2019-03-23 20:27:24  ...        Manhattan 0 days 00:06:15
-# 1  2019-03-04 16:11:55  2019-03-04 16:19:00  ...        Manhattan 0 days 00:07:05
+# s1 = [1,2,3,4,5,6,7,8,9,10]
+# s2 = [0,1,1,2,2,3,3,4,4,5,]
+# m, b = compute_r_line(s1,s2)
+# print(m,b)
+# xes = np.array([0,10])
+# yes = m*xes + b
+# plt.scatter(s1,s2)
+# plt.plot(xes,yes)
+# plt.title(f'Regression line with m = {m:{4}.{2}} and y-intercept = {b:{4}.{2}}')
+# plt.show()
