8cd6019db80e2cab097ed07d88edd93b8b9dbf9e
Ardi <ardiasllani@hotmail.com>
2021-10-26 11:45:15 -0400

    p29

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 """
 Name: Ardi Artani
 Email: ARDI.ARTANI96@myhunter.cuny.edu
-Resources: machinelearningmastery.com
+Resources: n/a
 """
 import pandas as pd
-import numpy as np
+from sklearn import linear_model
 
-# In Program 24, we wrote a function that counted courses that students are currently taking. For this program, write a function that takes a DataFrame and returns a sorted list of the computer science courses taken. Each course should occur once in the list, no matter how often it occurs in the list of courses taken by the students
+# the name of a CSV file that will be loaded into a dataframe,
+# the name of a column to be used as an independent variable in the multiple linear regression,
+# the name of second column to be used as another independent variable in the multiple linear regression,
+# the name of a column to be used as an dependent variable in the multiple linear regression (i.e. what is being predicted),
+# a value for the first variable, and
+# a value for the second variable.
 
-# csCourses(df): This function takes as input a data frame, df, that contains a column Current Courses. It returns a sorted list of unique strings. Each string is a computer science course (i.e. starts with csci) that occurs in some entry of df['Current Courses'].
-def csCourses(df):
-    results_ = df.loc[:,'Current Courses']
-    csci_ = []
-    for value_ in results_:
-        courses_ = value_.split()
-        for course_ in courses_:
-            if(course_.startswith('csci')):
-                csci_.append(course_)
+# get file names:
+csv_file_ = input('Enter name of CSV: ')
+ind_var_ = input('Enter name of first independent variable: ')
+ind_var2_ = input('Enter name of second independent variable: ')
+name_var_ = input('Enter name of the dependent variable:')
+pre_var_ = input('Enter value for first variable for prediction ')
+pre_var2_ = input('Enter value for second variable for prediction ')
 
-    results_ = sorted(np.unique(csci_))
-    return results_
+# Your program should build a linear model based on the dataframe and two independent variables that will predict the value of the dependent variable. Your program should then predict the dependent variable, based on the two inputted independent variables.
 
-# CS courses:
-#  ['csci150', 'csci160', 'csci235', 'csci265', 'csci335', 'csci39542', 'csci49362', 'csci499']
+# csv_file_ = "mpg.csv"
+# ind_var_ = "displacement"
+# ind_var2_ = "acceleration"
+# name_var_ = "mpq"
+# pre_var_ = 100
+# pre_var2_ = 12.0
+
+regr = linear_model.LinearRegression()
+regr.fit(mpg[[ind_var_, ind_var2_]], mpg[name_var_])
+print (f'Predicted mpg: {regr.predict([[pre_var_,pre_var2_]])[0]}')
