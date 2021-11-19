f6636647ce6f0e841c41538b53ba084c51a62185
Ardi <ardiasllani@hotmail.com>
2021-11-19 11:02:28 -0500

    p44

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Resources: p42 code
 """
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
 def compare_clf(xes, y, test_size = 0.20, random_state=66,max_iter=500):
