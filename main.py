26f44a2a85f9c61340f04af60c87d7a60347a172
Ardi <ardiasllani@hotmail.com>
2021-11-19 10:56:42 -0500

    p44

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ Resources: p42 hints, numpy.org
 """
 import numpy as np
 import pandas as pd
-import numpy.linalg as la
+from sklearn.svm import SVC
 
+def compare_clf(xes, y, test_size = 0.20, random_state=66,max_iter=500):
+    x_, x_test, y_, y_test = train_test_split(xes, y, random_state = random_state, test_size = test_size)
 
-# transition_mx: an square array of values between 0 and 1. Each column sums to 1.
-# starting_pop: an 1D array of positive numeric values that has the same length as transition_mx.
-# num_years: a column name containing the dependent variable (what's being predicted) in the model. It has a default value is 1.
-# The function returns an array of the population of each state after num_years.
-def moving(transition_mx, starting_pop, num_years = 1):
-    results_ = la.matrix_power(transition_mx, num_years) @ starting_pop
-    return results_
+    # grab LogisticRegression score
+    clf = LogisticRegression(max_iter = max_iter)
+    clf.fit(x_,y_)
+    logistic_score_ = clf.score(x_test, y_test)
 
+    # grab SVC score
+    clf_svc_ = SVC()
+    clf_svc_.fit(x_,y_)
+    svc_score_ = clf_svc_.score(x_test, y_test)
 
-# transition_mx: an square array of values between 0 and 1. Each row sums to 1.
-# starting_pop: an 1D array of positive numeric values that has the same length as transition_mx.
-# num_years: a column name containing the dependent variable (what's being predicted) in the model. It has a default value is 1.
-# The function returns an array of the population of each state at the steady state. That is, it returns the eigenvector corresponding to the largest eigenvalue, scaled so that it's entries sum to 1, and then multiplied by the sum of the starting populations.
-def steadyState(transition_mx, starting_pop):
-    w,v = la.eig(transition_mx)
-    total_ = v[:, 0]/sum(v[:, 0])
-    return (total_ * sum(starting_pop))
+    return logistic_score_, svc_score_
